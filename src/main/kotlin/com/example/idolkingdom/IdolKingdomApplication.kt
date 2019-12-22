d37e89f64f9000fe121b04665fe12dd9549f06a0
yeeeeerin <ebbunbul@gmail.com>
2019-12-22 19:14:29 +0900

    project init

---
+++ src/main/kotlin/com/example/idolkingdom/IdolKingdomApplication.kt
@@ -0,0 +1,11 @@
+package com.example.idolkingdom
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IdolKingdomApplication
+
+fun main(args: Array<String>) {
+    runApplication<IdolKingdomApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/idolkingdom/IdolKingdomApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.idolkingdom
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IdolKingdomApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
