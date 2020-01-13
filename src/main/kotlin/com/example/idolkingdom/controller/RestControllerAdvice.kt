e8b116c39e0413e1412ad2dc09b5cb5aff7571a9
yeeeeerin <ebbunbul@gmail.com>
2020-01-13 21:15:27 +0900

    Add exception advice

---
+++ src/main/kotlin/com/example/idolkingdom/controller/RestControllerAdvice.kt
@@ -0,0 +1,18 @@
+package com.example.idolkingdom.controller
+
+import com.example.idolkingdom.exception.UserNotFoundException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class RestControllerAdvice {
+
+    @ExceptionHandler(UserNotFoundException::class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    fun handleUserNotFoundException(ex: Exception): ResponseEntity<Any?> {
+        return ResponseEntity(ex.message, HttpStatus.NOT_FOUND)
+    }
+}
