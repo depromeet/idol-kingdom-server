35593e750b0ede6126e90886fda77dd98a37b3f4
yeeeeerin <ebbunbul@gmail.com>
2020-01-13 21:16:19 +0900

    Fix user model

--- src/main/kotlin/com/example/idolkingdom/model/User.kt
+++ src/main/kotlin/com/example/idolkingdom/model/User.kt
@@ -1,21 +1,24 @@
 package com.example.idolkingdom.model
 
-import java.util.*
+import lombok.AccessLevel
+import lombok.NoArgsConstructor
+import java.time.LocalDateTime
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
 
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 data class User(
+    @Id @GeneratedValue
     val id: Int,
+
+    val email: String,
+    val password: String,
     val name: String,
+
     val school: School,
-    val account: Account,
     val lovedGroups: List<IdolGroup>,
     val votedList: List<Vote>,
-    val createdAt: Date
-) {
-
-    data class Account(
-        val email: String,
-        val password: String
-//        val kakaoId: String,
-//        val facebookId: String
-    )
-}
+    val createdAt: LocalDateTime
+) {}
