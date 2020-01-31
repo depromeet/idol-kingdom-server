0f7a44978712701a086fd6aed3a79d7c2d049ea5
yeeeeerin <ebbunbul@gmail.com>
2020-01-31 14:29:43 +0900

    Apply 에러 수정

--- src/main/kotlin/com/example/idolkingdom/service/IdolService.kt
+++ src/main/kotlin/com/example/idolkingdom/service/IdolService.kt
@@ -1,13 +1,13 @@
 package com.example.idolkingdom.service
 
 
-import com.example.idolkingdom.dto.IdolGroupResponseDto
 import com.example.idolkingdom.dto.IdolDto
+import com.example.idolkingdom.dto.IdolGroupResponseDto
 import com.example.idolkingdom.model.IdolGroup
 
 interface IdolService {
     fun getAll(): List<IdolGroup>
     fun get(name: String): List<IdolGroup>
-    fun search(query: String): List<IdolGroup>
+    fun search(query: String): List<IdolDto>
     fun get(idolGroup: Long): IdolGroupResponseDto
 }
