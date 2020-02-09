7605c7c14fbf2499edef845de9ed4a8f264cc40c
yeeeeerin <ebbunbul@gmail.com>
2020-02-10 02:56:38 +0900

    Apply 필요없는 함수 삭제

--- src/main/kotlin/com/example/idolkingdom/util/IdolImage.kt
+++ src/main/kotlin/com/example/idolkingdom/util/IdolImage.kt
@@ -8,12 +8,6 @@ enum class IdolImage(private val idolName: String) {
     DALSHABET("달샤벳");
 
     companion object {
-//        fun haveIdol(name: String): Boolean {
-//            var isHaveIdol = false
-//            values().filter { idolImage -> idolImage.idolName == name }
-//                .firstOrNull()
-//
-//        }
 
         fun getIdolGroupByName(name: String): IdolImage? = values().firstOrNull { idolImage -> idolImage.idolName == name }
 
